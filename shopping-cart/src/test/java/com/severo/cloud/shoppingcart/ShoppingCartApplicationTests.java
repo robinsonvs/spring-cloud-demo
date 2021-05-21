baaf1ac082ec11830b305c7f307504b6f01cfb06
robinsonvs <robinsonvs@gmail.com>
2021-05-21 15:54:39 -0300

    shopping-cart start

---
+++ shopping-cart/src/test/java/com/severo/cloud/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.severo.cloud.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
