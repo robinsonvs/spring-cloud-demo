eddfe9c396177bb05856e93aff264da159a256be
robinsonvs <robinsonvs@gmail.com>
2021-05-21 16:11:14 -0300

    shopping-cart add repository and models

--- shopping-cart/src/main/java/com/severo/cloud/shoppingcart/repository/ShoppingCartRepository.java
+++ shopping-cart/src/main/java/com/severo/cloud/shoppingcart/repository/ShoppingCartRepository.java
@@ -1,6 +1,8 @@
 package com.severo.cloud.shoppingcart.repository;
 
+import com.severo.cloud.shoppingcart.model.Cart;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ShoppingCartRepository extends CrudRepository<> {
+public interface ShoppingCartRepository extends CrudRepository<Cart, Integer> {
+    Cart findByCustomerId(Integer customerId);
 }
